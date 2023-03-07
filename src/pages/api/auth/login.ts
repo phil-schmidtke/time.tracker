f07ca02fe1ef8995e11e77dc72093e8361715838
Phil Schmidtke <mail@phil-schmidtke.de>
2023-03-07 12:52:56 +0000

    Update login.ts

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -4,14 +4,9 @@ import prisma from "../../../lib/prisma";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-type Data = {
-  token: string | null;
-  error: string | null;
-};
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   if (req.method === "POST") {
     const username = req.body.username;
