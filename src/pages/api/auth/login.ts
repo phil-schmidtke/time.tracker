80960922998ae50ccc54ddf5f4d55816a70224d2
Phil Schmidtke <mail@phil-schmidtke.de>
2023-03-07 11:58:35 +0000

    first logic

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,14 +1,40 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
+import bcrypt from "bcrypt";
 
 type Data = {
-  name: string
-}
+  token: string | null;
+  error: string | null;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === "POST") {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+      const hashPw = await bcrypt.hash(password, 10);
+      console.log("hashpw", hashPw);
+      const user = await prisma.user.findFirst({
+        where: {
+          Username: username,
+          Password: hashPw,
+        },
+      });
+      console.log("user", user);
+      if (user) {
+        res.status(200).json({ username, password });
+      } else {
+        res.status(400).json({ error: "no user found" });
+      }
+    } else {
+      res.status(400).json({ error: "Wrong JSON Body" });
+    }
+  } else {
+    res.status(404).json({ error: "Unsupported HTTP Method" });
+  }
+  res.status(200).json({ name: "John Doe" });
 }
--
--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,14 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
+import bcrypt from "bcrypt";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+      const hashPw = await bcrypt.hash(password, 10);
+      const user = await prisma.user.create({
+        data: {
+          Mail: email,
+          Username: username,
+          Password: hashPw,
+        },
+      });
+      if (user) {
+        res.status(200).json({ message: "User created Successfully" });
+      } else {
+        res.status(400).json({ error: "no user found" });
+      }
+    } else {
+      res.status(400).json({ error: "Wrong JSON Body" });
+    }
+  } else {
+    res.status(404).json({ error: "Unsupported HTTP Method" });
+  }
 }
--
